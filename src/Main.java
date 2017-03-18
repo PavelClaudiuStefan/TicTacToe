dab7eb0225249eda92d4e805c004dc314155154e
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2017-03-18 11:46:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import game.TicTacToe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TicTacToe game = new TicTacToe();
+		game.start();
+
+	}
+
+}
---
+++ src/game/TicTacToe.java
@@ -0,0 +1,7 @@
+package game;
+
+public class TicTacToe {
+	public void start() {
+		System.out.println("Starting game");
+	}
+}
